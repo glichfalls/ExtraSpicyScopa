bba9f9cc9ceac88054fb0ee4d2b744945c34d8cb
Robin Portner <robin.portner@novu.ch>
2026-03-12 10:55:29 +0100

    feat: added landing page

---
+++ src/Controller/LandingController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class LandingController extends AbstractController
+{
+    #[Route('/', name: 'landing')]
+    public function index(): Response
+    {
+        return $this->render('landing.html.twig', [
+            'bot_username' => $_ENV['TELEGRAM_BOT_USERNAME'] ?? 'ExtraSpicyStickersBot',
+        ]);
+    }
+}
